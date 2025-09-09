437d9ff1773bd9617b5b423f274a4f72c8efb750
Manish Kumar <mk3176553@gmail.com>
2025-09-09 08:46:51 +0530

    Updated chat app with read, edit and delete functionality

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const port = 8080;
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 
 //DataBase Creating
@@ -55,8 +57,32 @@ app.post("/chats", (req, res) => {
 
 
 //Read route
-app.get("/")
+app.get("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("show.ejs", { chat });
+});
 
+//Edit Route
+app.get("/chats/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", { chat });
+});
+
+app.patch("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    let { msg } = req.body;
+    await Chat.findByIdAndUpdate(id, { msg: msg }, { runValidators: true, new: true });
+    res.redirect("/chats");
+});
+
+//Delete Route
+app.delete("/chats/:id", async (req, res) => {
+    let { id } = req.params;
+    await Chat.findByIdAndDelete(id);
+    res.redirect("/chats");
+});
 
 // App Set Up
 app.get("/", (req, res) => {
--- init.js
+++ init.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
 const Chat = require("./models/chat.js");
 
+
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/chatapp');
     console.log("Database Connected Successfully");
 
     let allChats = [
         {
+            
             from: "manish",
             to: "hello",
             msg: "hello love you",
             created_at: new Date()
         },
         {
+            
             from: "simran",
             to: "manish",
             msg: "hello miss you",
